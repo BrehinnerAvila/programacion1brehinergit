fdfa955dac08b3581e98e3c2f873f38a994976de
Brehinner Avila <brehinneravila125@unisangil.edu.co>
2025-09-03 14:55:34 -0500

    operadores de comparacion

---
+++ operadore_comparacion.py
@@ -0,0 +1,13 @@
+num1=5
+num2=10
+print(num1==num2)
+#diferente
+print(num1!=num2)
+#mayor que 
+print(num1>num2)
+#menor que 
+print(num1<num2)
+#mayor o igual 
+print(num1>=num2)
+#menoro igual
+print(num1<=num1)
